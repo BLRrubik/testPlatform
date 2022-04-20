e5d151ac787e79bee9cfb848acf5306baead52f3
blrrubik <rubromvit@gmail.com>
2022-04-20 18:28:27 +0300

    init commit

---
+++ src/main/java/ru/vibelab/tplatfom/TestPlatfomApplication.java
@@ -0,0 +1,13 @@
+package ru.vibelab.tplatfom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPlatfomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPlatfomApplication.class, args);
+    }
+
+}
