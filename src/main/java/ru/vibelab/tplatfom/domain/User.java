723de23e302bd285105da6869886bc877f98be29
Mathless <mrkalumba@gmail.com>
2022-04-24 20:14:41 +0300

    User entity

---
+++ src/main/java/ru/vibelab/tplatfom/domain/User.java
@@ -0,0 +1,26 @@
+package ru.vibelab.tplatfom.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    
+}
