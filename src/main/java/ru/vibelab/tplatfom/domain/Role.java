6eff244b683d49ff0f7d8d53b6bc702b250db78d
Yegor <gare577@gmail.com>
2022-04-25 14:08:16 +0300

    Добавить модели User, Role

---
+++ src/main/java/ru/vibelab/tplatfom/domain/Role.java
@@ -0,0 +1,24 @@
+package ru.vibelab.tplatfom.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ src/main/java/ru/vibelab/tplatfom/domain/User.java
@@ -0,0 +1,27 @@
+package ru.vibelab.tplatfom.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
