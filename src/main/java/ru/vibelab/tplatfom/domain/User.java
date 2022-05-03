f28fb5945cc22eed68add03b2dc9faf459bb99bd
Mathless <mrkalumba@gmail.com>
2022-05-03 17:04:45 +0300

    Добавил EAGER в User, но это решение не для продакшна

--- src/main/java/ru/vibelab/tplatfom/domain/User.java
+++ src/main/java/ru/vibelab/tplatfom/domain/User.java
@@ -27,7 +27,7 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany()
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
