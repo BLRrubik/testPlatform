0336e118cc6cb05d3811277e4bfd090c00dde950
Yegor <gare577@gmail.com>
2022-05-04 14:43:26 +0300

    Удалить лишний код

--- src/main/java/ru/vibelab/tplatfom/TestPlatformApplication.java
+++ src/main/java/ru/vibelab/tplatfom/TestPlatformApplication.java
@@ -2,9 +2,8 @@ package ru.vibelab.tplatfom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })  // TODO: Вернуть авторизацию
+@SpringBootApplication
 public class TestPlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestPlatformApplication.class, args);
